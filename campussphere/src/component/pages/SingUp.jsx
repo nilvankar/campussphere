54f8555a7d901d1a173f8354d822d2ffbef39c43
Swayam Vasavada <swayam.vasavada1505@gmail.com>
2024-02-03 20:03:27 +0530

    Fixed styling

--- campussphere/src/component/pages/SingUp.jsx
+++ campussphere/src/component/pages/SingUp.jsx
@@ -4,6 +4,9 @@ import "../../assets/styles/signup.css";
 import { useNavigate } from "react-router-dom";
 import Footer from "../layout/Footer";
 import Header from "../layout/Header";
+import "../../assets/styles/base.css"
+import "../../assets/styles/forms.css"
+
 export default function SingUp() {
   const initialState = {
     firstName: " ",
@@ -18,7 +21,7 @@ export default function SingUp() {
     pincode: " ",
     gender: { male: false, female: false },
     primaryNo: " ",
-    secondaryNumber: " ",
+    alternativeNo: " ",
     desg: {
       admin: false,
       student: false,
@@ -43,11 +46,11 @@ export default function SingUp() {
     },
   };
   const [signUp, setSignUp] = useState(initialState);
-     // token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7I…zOTZ9.Bm8foD4RI_PjRcDqL4XX20bIUHqVWtEzxejZzr1f3B4'
-     const [formData, setFormData] = useState(initialState);
-     const [fetchUser, setFetchUser] = useState(null);
-     const [authToken, setAuthToken] = useState(null);
-  const navigate=useNavigate();   
+  // token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7I…zOTZ9.Bm8foD4RI_PjRcDqL4XX20bIUHqVWtEzxejZzr1f3B4'
+  const [formData, setFormData] = useState(initialState);
+  const [fetchUser, setFetchUser] = useState(null);
+  const [authToken, setAuthToken] = useState(null);
+  const navigate = useNavigate();
   const handleChange = (e) => {
     const { name, type, value } = e.target;
     if (type === "checkbox") {
@@ -61,7 +64,7 @@ export default function SingUp() {
     } else {
       setSignUp({ ...signUp, [name]: value });
     }
-    
+
     setFormData({ ...formData, [name]: value });
   };
   const {
@@ -79,7 +82,7 @@ export default function SingUp() {
     batch,
     dept,
     primaryNo,
-    secondaryNumber,
+    alternativeNo,
   } = signUp;
 
 
@@ -98,14 +101,14 @@ export default function SingUp() {
     batch,
     dept,
     primaryNo,
-    secondaryNumber,
+    alternativeNo,
   };
 
-  const handleSubmit=async (e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     try {
-      const storedToken=localStorage.getItem('authToken');
+      const storedToken = localStorage.getItem('authToken');
       const response = await fetch(
         "https://blcdjd0s-5000.inc1.devtunnels.ms/create-user",
         {
@@ -117,16 +120,16 @@ export default function SingUp() {
           body: JSON.stringify(formData),
         }
       );
-  
+
       if (response.ok) {
         const userData = await response.json();
         const jwtToken = userData.token;
-        
+
         // Corrected line: use setItem method to store the token in local storage
-  
+
         setFetchUser(userData);
         navigate('/');
-        console.log("singup successful :", userData);        
+        console.log("singup successful :", userData);
       } else {
         console.error("singup failed:", response.statusText);
         // Handle the error, e.g., set an error state or display a message to the user
@@ -139,332 +142,159 @@ export default function SingUp() {
   };
 
   // https://blcdjd0s-5000.inc1.devtunnels.ms/create-user  ----------------------------->forwared port
-return (
-  <div className="signup" style={{height:"410vh",width:"100vw"}}>
-  <Header />
-  <div className="main" style={{height:"300vh"}}>
-  
-   
-{  /*    <div className="img-container">
-        <img src={contactImg} alt="Login Image" className="log-img" />
-      </div>
-    
-*/}
-      <div className="form-container" style={{height:"200vh"}}>
+  return (
+    <div className="signup" style={{ height: "100%", width: "100vw" }}>
+      <Header />
+      <div className="main">
+        <h1 className="">Register Account</h1>
+        <form action="">
 
-        <form action="https://blcdjd0s-5000.inc1.devtunnels.ms/create-user" method="POST" className="stuForm"
-          onSubmit={handleSubmit}
-        >
-          <h1 className="text-center font-bold">Sign Up</h1>
-          <div className="form-div " style={{width:"42vw"}}>
-            <label
-              htmlFor="email"
-              className="block text-gray-700 text-sm font-medium"
-            >
-              Email
-            </label>
-            <input
-              className="form-input" style={{width:"33vw"}}
-              type="email"
-              name="email"
-              id="email"
-              required
-              onChange={handleChange}
-            />
-          </div>
-          <div className="form-div " style={{width:"42vw"}}>
-            <label
-              htmlFor="password"
-              className="block text-gray-700 text-sm font-medium"
-            >
-              password
-            </label>
-            <input
-              className="form-input" style={{width:"33vw"}}
-              type="password"
-              name="password"
-              id="password"
-              required
-              onChange={handleChange}
-            />
-          </div>
-          <div className="form-div " style={{width:"42vw"}}>
-            <label
-              htmlFor="firstName"
-              className="block text-gray-700 text-sm font-medium"
-            >
-              First Name
-            </label>
-            <input
-              className="form-input" style={{width:"33vw"}}
-              type="text"
-              name="firstName"
-              id="firstName"
-              required
-              onChange={handleChange}
-            />
-          </div>
-          <div className="form-div " style={{width:"42vw"}}>
-            <label
-              htmlFor="lastName"
-              className="block text-gray-700 text-sm font-medium"
-            >
-              Last Name
-            </label>
-            <input
-              className="form-input" style={{width:"33vw"}}
-              type="text"
-              name="lastName"
-              id="lastName"
-              required
-              onChange={handleChange}
-            />
-          </div>
-          <div className="form-div " style={{width:"42vw"}}>
-            <label htmlFor="dob">Date Of Birth</label>
-            <input
-              className="form-input" style={{width:"33vw"}} type="date" name="dob" id="dob" onChange={handleChange} />
-          </div>
-          <div className="form-div " style={{width:"42vw"}}>
-            <label
-              htmlFor="fatherName"
-              className="block text-gray-700 text-sm font-medium"
-            >
-              Father 's Name
-            </label>
-            <input
-              className="form-input" style={{width:"33vw"}}
-              type="text"
-              name="fatherName"
-              id="fatherName"
-              required
-              onChange={handleChange}
-            />
-          </div>
-          <div className="form-div " style={{width:"42vw"}}>
-            <label
-              htmlFor="colonyName"
-              className="block text-gray-700 text-sm font-medium"
-            >
-              colonyName
-            </label>
-            <input
-              className="form-input" style={{width:"33vw"}}
-              type="text"
-              name="colonyName"
-              id="colonyName"
-              required
-              onChange={handleChange}
-            />
-          </div>
-          <div className="form-div " style={{width:"42vw"}}>
-            <label
-              htmlFor="landmark"
-              className="block text-gray-700 text-sm font-medium"
-            >
-              Landmark
-            </label>
-            <input
-              className="form-input" style={{width:"33vw"}}
-              type="text"
-              name="landmark"
-              id="landmark"
-              required
-              onChange={handleChange}
-            />
-          </div>
-          <div className="form-div " style={{width:"42vw"}}>
-            <label
-              htmlFor="area"
-              className="block text-gray-700 text-sm font-medium"
-            >
-              Area
-            </label>
-            <input
-              className="form-input" style={{width:"33vw"}}
-              type="text"
-              name="area"
-              id="area"
-              required
-              onChange={handleChange}
-            />
-          </div>
-          <div className="form-div " style={{width:"42vw"}}>
-            <label
-              htmlFor="city"
-              className="block text-gray-700 text-sm font-medium"
-            >
-              City
-            </label>
-            <input
-              className="form-input" style={{width:"33vw"}}
-              type="text"
-              name="city"
-              id="city"
-              required
-              onChange={handleChange}
-            />
+          <div>
+            Personal Info
+            <hr />
           </div>
 
-          <div className="form-div " style={{width:"42vw"}}>
-            <label
-              htmlFor="pincode"
-              className="block text-gray-700 text-sm font-medium"
-            >
-              Pincode
-            </label>
-            <input
-              className="form-input" style={{width:"33vw"}}
-              type="number"
-              name="pincode"
-              id="pincode"
-              required
-              onChange={handleChange}
-            />
+          <div className="form-control multi-input">
+
+            <div>
+              <label htmlFor="firstName">Enter firstname</label>
+              <input type="text" name="firstName" id="firstName" />
+            </div>
+
+            <div>
+              <label htmlFor="lastName">Enter lastname</label>
+              <input type="text" name="lastName" id="lastName" />
+            </div>
+
           </div>
-          <div className="form-div "style={{ width: "42vw", height: "11vh", marginTop: "9px", fontWeight: "bold" }}>
-            <label htmlFor="gender">Gender</label>
-            <select 
-              name="gender"
-              id="gender"
-              required
-              className="block text-gray-700 text-sm font-medium"
-              onChange={handleChange}
-            >
 
-              <option hidden > Select Gender</option>
+          <div className="form-control">
+            <label htmlFor="fatherName">Enter your father name</label>
+            <input type="text" name="fatherName" id="fatherName" />
+          </div>
 
-              <option value="male" onChange={handleChange}>
-                Male
-              </option>
-              <option value="female" onChange={handleChange}>
-                Female
-              </option>
+          <div className="form-control">
+            <label htmlFor="gender">Select Gender</label>
+            <select name="gender" id="gender">
+              <option hidden>Select your Gender</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
             </select>
           </div>
 
-          <div className="form-div " style={{width:"42vw"}}>
-            <label
-              htmlFor="primaryNo"
-              className="block text-gray-700 text-sm font-medium"
-            >
-              Contact Number
-            </label>
-            <input
-              className="form-input" style={{width:"33vw"}}
-              type="number"
-              name="primaryNo"
-              id="primaryNo"
-              required
-              onChange={handleChange}
-            />
+          <div className="form-control multi-input">
+
+            <div>
+              <label htmlFor="primaryNo">Enter primary number</label>
+              <input type="tel" name="primaryNo" id="primaryNo" />
+            </div>
+
+            <div>
+              <label htmlFor="alternativeNo">Enter alternative number</label>
+              <input type="tel" name="alternativeNo" id="alternativeNo" />
+            </div>
           </div>
-          <div className="form-div " style={{width:"42vw"}}>
-            <label
-              htmlFor="alternativeNo"
-              className="block text-gray-700 text-sm font-medium"
-            >
-              Alternative Number
-            </label>
-            <input
-              className="form-input" style={{width:"33vw"}}
-              type="number"
-              name="alternativeNo"
-              id="alternativeNo"
-              onChange={handleChange}
-            />
+
+          <div className="form-control">
+            <label htmlFor="dob">Pick your date of birth</label>
+            <input type="date" name="dob" id="dob" />
           </div>
-          <div className="form-div " style={{width:"42vw"}}>
-            <select 
-              name="desg"
-              id="desg"
-              required
-              className="block text-gray-700 text-sm font-medium"
-              onChange={handleChange}
-            >
+
+          <div>
+            Residential info
+            <hr />
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="colonyName">Enter Colony name</label>
+            <input type="text" name="colonyName" id="colonyName" placeholder="House no, Flat name/Society Name" />
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="landmark">Enter landmark</label>
+            <input type="text" name="landmark" id="landmark" placeholder="Opp XYZ, Near ABC" />
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="area">Enter Colony name</label>
+            <input type="text" name="area" id="area" placeholder="Ex Wagodia Road" />
+          </div>
+
+          <div className="form-control multi-input">
+            <div>
+              <label htmlFor="city">Enter City</label>
+              <input type="text" name="city" id="city" placeholder="Ex Vadodara" />
+            </div>
+
+            <div>
+
+              <label htmlFor="pincode">Enter Pincode</label>
+              <input type="number" name="pincode" id="pincode" placeholder="Ex 390019" />
+            </div>
+          </div>
+
+          <div>
+            Login Info
+            <hr />
+
+            <span>The provided info will be used for Login your ERP</span>
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="email">Enter email</label>
+            <input type="email" name="email" id="email" />
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="password">Enter password</label>
+            <input type="password" name="password" id="password" />
+          </div>
+
+
+          <div>
+            Academic Info
+            <hr />
+          </div>
+
+          <div className="form-control multi-input">
+
+            <div>
+              <label htmlFor="dept">Select department</label>
+              <select name="dept" id="dept">
+                <option hidden>Select your department</option>
+                <option value="CSE">CSE</option>
+                <option value="mechanical">Mechanical</option>
+                <option value="civil">Civil</option>
+                <option value="mechatronics">Mechatronics</option>
+              </select>
+            </div>
+
+            <div>
+              <label htmlFor="batch">Select batch</label>
+              <select name="batch" id="batch">
+                {/* It should be rendered dynamically */}
+                <option hidden>Select batch</option>
+                <option value="2021">2021</option>
+                <option value="2022">2022</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="desg">Select Designation</label>
+            <select name="desg" id="desg">
               <option hidden>Select Designation</option>
-              <option value="admin" onChange={handleChange}>
-                Administrator
-              </option>
-              <option value="student" onChange={handleChange}>
-                Student
-              </option>
-              <option value="hod" onChange={handleChange}>
-                Head Of Department
-              </option>
-              <option value="faculty" onChange={handleChange}>
-                Faculty
-              </option>
+              <option value="student">Student</option>
+              <option value="faculty">Faculty</option>
+              <option value="hod">HOD</option>
+              <option value="Admin">Admin</option>
             </select>
           </div>
-          <div className="form-div " style={{width:"42vw"}}>
-            <select 
-              name="batch"
-              id="batchsel"
-              className="block text-gray-700 text-sm font-medium"
-            >
-              <option value="batchsel" hidden onChange={handleChange}>
-                {" "}
-                Select Batch
-              </option>
-              <option value="2024" onChange={handleChange}>
-                2024
-              </option>
-              <option value="2023" onChange={handleChange}>
-                2023
-              </option>
-              <option value="2022" onChange={handleChange}>
-                2022
-              </option>
-              <option value="2021" onChange={handleChange}>
-                2021
-              </option>
-              <option value="2020" onChange={handleChange}>
-                2020
-              </option>
-              <option value="2019" onChange={handleChange}>
-                2019
-              </option>
-              <option value="2018" onChange={handleChange}>
-                2018
-              </option>
-              <option value="2017" onChange={handleChange}>
-                2017
-              </option>
-            </select>
-          </div>
-          <div className="form-div " style={{width:"42vw"}}>
-            <select  name="dept" id="dept">
-              <option value="dept" hidden onChange={handleChange}>
-                Select Department
-              </option>
-              <option value="computerScience" onChange={handleChange}>
-                CSE
-              </option>
-              <option value="civil" onChange={handleChange}>
-                Civil
-              </option>
-              <option value="mechanical" onChange={handleChange}>
-                Mechanical
-              </option>
-              <option value="mechatronics" onChange={handleChange}>
-                Mechatronics
-              </option>
-            </select>
-          </div>
-          <div className="form-div " style={{width:"42vw"}}>
-            <input
-              className="form-input bg-sky-300/95 hover:bg-sky-600/100 text-white" style={{width:"20vw", marginLeft:"95px"}}
-              type="submit"
-              value="submit"
-            
-            />
-          </div>
+
+
         </form>
       </div>
+      <Footer />
     </div>
- 
-  <Footer />
-  </div>
-);
+  );
 }
